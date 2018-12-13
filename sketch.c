b8b6b9565ac9ca4237c114bfb5a1d790206b5cff
Dan Watkinson <dan.watco@gmail.com>
2018-12-13 16:10:44 +0000

    Created and tested opcode func

--- sketch.c
+++ sketch.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <assert.h>
 
 enum OPCODE {DX, DY, FF, PEN};
 
 int getOpcode(uint8_t byte){
-    return 0;
+    return (byte & 0xC0) >> 6;
 }
 
+void testOp(){
+    unsigned char test = 0x7D;
+    assert(getOpcode(test) == 1);
+    test = 0xC0;
+    assert(getOpcode(test) == 3);
+    test = 0x03;
+    assert(getOpcode(test) == 0);
+}
 
 int main(){
+    testOp();
+    printf("Tests pass\n");
     return 0;
 }
