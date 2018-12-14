681ecdc39d625c6eb4229f856c85514b8815667f
Dan Watkinson <dan.watco@gmail.com>
2018-12-14 11:32:02 +0000

    Tested display module

--- displaytest.c
+++ displaytest.c
@@ -6,7 +6,10 @@
 
 int main(){
     display *d = newDisplay("Test", 500, 500);
-    line(d, 0, 0, 250, 250);
+    line(d, 1, 1, 250, 250);
+    line(d, 500, 0, 250, 250);
+    pause(d, 5000);
+    clear(d);
     end(d);
     return 0;
 }
