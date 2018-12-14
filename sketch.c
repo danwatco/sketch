67efcef0897b9e82bdde80f61018309e94185d42
Dan Watkinson <dan.watco@gmail.com>
2018-12-14 12:50:17 +0000

    Added new opcodes and handlers for them

--- sketch.c
+++ sketch.c
@@ -5,7 +5,7 @@
 #include "display.h"
 #include <stdbool.h>
 
-enum OPCODE {DX, DY, FF, PEN};
+enum OPCODE {DX, DY, PR, DO};
 
 struct state {
     display *d;
@@ -13,6 +13,9 @@ struct state {
     int ypos;
     int dx;
     bool pen;
+    int operandval;
+    bool opinit;
+    int dt;
 };
 typedef struct state state;
 
@@ -27,7 +30,7 @@ int getOperand(unsigned char byte){
 state *initalise(char *path){
     display *d = newDisplay(path, 300, 300);
     state *s = malloc(sizeof(state));
-    *s = (state) {d, 0, 0, 0, false};
+    *s = (state) {d, 0, 0, 0, false, 0, false};
     return s;
 }
 
@@ -37,22 +40,55 @@ void finish(state *s){
 }
 
 void handleX(state *s, unsigned char byte){
-    int value = getOperand(byte);
-    s->dx = value;
+    if(s->opinit){
+        int value = s->operandval;
+        s->dx = (value << 6) | getOperand(byte);
+        s->operandval = 0;
+        s->opinit = false;
+    } else {
+        int value = getOperand(byte);
+        s->dx = value;
+    }
+    
 }
 
 void handleY(state *s, unsigned char byte){
-    int value = getOperand(byte);
-    if(s->pen){
-        display *d = s->d;
-        line(d, s->xpos, s->ypos, s->xpos + s->dx, s->ypos + value);
-        s->xpos = s->xpos + s->dx;
-        s->dx = 0;
-        s->ypos = s->ypos + value;
+    if(s->opinit){
+        
     } else {
-        s->xpos = s->xpos + s->dx;
-        s->dx = 0;
-        s->ypos = s->ypos + value;
+        int value = getOperand(byte);
+        if(s->pen){
+            display *d = s->d;
+            line(d, s->xpos, s->ypos, s->xpos + s->dx, s->ypos + value);
+            s->xpos = s->xpos + s->dx;
+            s->dx = 0;
+            s->ypos = s->ypos + value;
+        } else {
+            s->xpos = s->xpos + s->dx;
+            s->dx = 0;
+            s->ypos = s->ypos + value;
+        }
+    }
+    
+}
+
+void handlePR(state *s, unsigned char byte){
+    int value = 0;
+    if(!s->opinit){
+        s->opinit = true;
+        value = getOperand(byte);
+        s->operandval = value;
+    } else {
+        value = getOperand(byte);
+        s->operandval = (s->operandval << 6) | value;
+    }
+}
+
+void handleDO(state *s, unsigned char byte){
+    switch(getOperand(byte)){
+        case 0:
+            s->pen = ! (s->pen);
+            break;
     }
 }
 
@@ -65,8 +101,11 @@ void update(state *s, FILE *in){
         case DY:
             handleY(s, byte);
             break;
-        case PEN:
-            s->pen = ! (s->pen);
+        case PR:
+            handlePR(s, byte);
+            break;
+        case DO:
+            handleDO(s, byte);
             break;
     }
 }
