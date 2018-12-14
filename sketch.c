3089e1649f9d534e941efeb1059f7bf0dcb26f23
Dan Watkinson <dan.watco@gmail.com>
2018-12-14 11:53:23 +0000

    Create init and end functions

--- sketch.c
+++ sketch.c
@@ -24,6 +24,31 @@ int getOperand(unsigned char byte){
     return (signed char) ((byte & 0x3F) << 2 )>> 2;
 }
 
+state *initalise(char *path){
+    display *d = newDisplay(path, 300, 300);
+    state *s = malloc(sizeof(state));
+    *s = (state) {d, 0, 0, 0, false};
+    /*FILE *in = fopen(path, "r");
+    unsigned char byte = fgetc(in);
+    int op = getOpcode(byte);
+    int opnd = getOperand(byte);
+    printf("Opcode is %d and operand is %d\n", op, opnd);
+    finish(s);*/
+    return s;
+}
+
+void finish(state *s){
+    end(s->d);
+    free(s);
+}
+
+void run(char *path){
+    state *s = initalise(path);
+
+
+    finish(s);
+}
+
 void testOp(){
     unsigned char test = 0x7D;
     assert(getOpcode(test) == 1);
@@ -40,9 +65,18 @@ void testOper(){
     assert(getOperand(test) == 3);
 }
 
-int main(){
+void test(){
     testOp();
     testOper();
+    run("lines.sketch");
     printf("Tests pass\n");
+}
+
+int main(int n, char *args[n]){
+    if(n==1) test();
+    else {
+
+    }
+    
     return 0;
 }
